4e033e8989f2cfcf85616dc67094c3614186cd6b
Lucatanga10 <lucatanga010@gmail.com>
2026-03-07 01:09:55 +0100

    Update keep_alive.py to set default port and daemonize thread

--- keep_alive.py
+++ keep_alive.py
@@ -9,9 +9,10 @@ def home():
     return "Bot is alive!"
 
 def run():
-    port = int(os.environ['PORT'])
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
